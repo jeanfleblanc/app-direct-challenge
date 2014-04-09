8faff88ebb83277f8c6842c005615d7049b345af
jeanfleblanc <jeanfleblanc@gmail.com>
2014-04-08 21:43:41 -0400

    Add error codes

--- src/main/java/ca/leblanc/appdirect/controller/EventController.java
+++ src/main/java/ca/leblanc/appdirect/controller/EventController.java
@@ -3,21 +3,25 @@ package ca.leblanc.appdirect.controller;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import oauth.signpost.OAuthConsumer;
-import oauth.signpost.basic.DefaultOAuthConsumer;
+import javax.servlet.http.HttpServletRequest;
 
+import org.miniauth.exception.ValidationException;
+import org.miniauth.oauth.credential.mapper.OAuthSingleConsumerCredentialMapper;
+import org.miniauth.web.oauth.OAuthProviderAuthHandler;
+import org.miniauth.web.oauth.OAuthSingleConsumerURLConnectionAuthHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import ca.leblanc.appdirect.domain.EventErrorCode;
 import ca.leblanc.appdirect.domain.Result;
 
-@Controller()
+@RestController()
 public class EventController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(EventController.class);
@@ -27,46 +31,77 @@ public class EventController {
      * 
      * <p>Expected HTTP GET and request '/event/buy'.</p>
      */
-    @RequestMapping(value="/event/subscriptionOrder", method=RequestMethod.GET)
-	public @ResponseBody Result subscriptionOrder(@RequestParam("eventUrl") String eventUrl) throws Exception  {
+    @RequestMapping(value="/event/subscriptionOrder", method=RequestMethod.GET, produces=MediaType.APPLICATION_XML_VALUE)
+	public Result subscriptionOrder(HttpServletRequest request, @RequestParam("eventUrl") String eventUrl) throws Exception  {
 		
+    	Result result;
+    	
     	// log everything!
     	logger.info("************** Entering method");
-    	 
-    	// TODO: verify encryption
+ 
+       
     	
-    	// callback
-    	OAuthConsumer consumer = new DefaultOAuthConsumer("bijoux-8197", "RHDwlCp4EhN6Mtmm");
-    	
-    	logger.info("************** opening url:" + eventUrl);
-    	URL url = new URL(eventUrl);
-    	HttpURLConnection request = (HttpURLConnection) url.openConnection();
-    	consumer.sign(request);
-    	request.connect();    	
-
-    	logger.info("About to save!");
-
-    	
-    	// TODO: save order
-    	
-    	// read xml
-    	String type = request.getContentType();
-    	
-    	logger.info("Content type is " + type);
-    	
-    	// send answer:
-    	/*
-    	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-    	<result>
-    	    <success>true</success>
-    	    <message>Account creation successful</message>
-    	    <accountIdentifier>new-account-identifier</accountIdentifier>
-    	</result>
-*/
-    	//or for errors:http://info.appdirect.com/developers/docs/event_references/api_error_codes/ 
-    	
-    	Result result = new Result(true, "Account creation successful", "fake124");
-    	
+    	// Verify signature       	
+       	OAuthSingleConsumerCredentialMapper mapper = new OAuthSingleConsumerCredentialMapper("bijoux-8197", "RHDwlCp4EhN6Mtmm");
+       	OAuthProviderAuthHandler oauthProviderAuthHandler = new OAuthProviderAuthHandler(mapper);
+       	boolean validSignature;
+       	
+       	try {
+       		
+       		validSignature = oauthProviderAuthHandler.verifyRequest(request);
+       	} catch (ValidationException e) {
+       			
+       		validSignature = false;
+       	}
+       	
+       	if (validSignature) {
+       	
+	       	// Connect to retrieve info
+	       	URL url = new URL(eventUrl);
+	       	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	       	OAuthSingleConsumerURLConnectionAuthHandler handler = new OAuthSingleConsumerURLConnectionAuthHandler("bijoux-8197", "RHDwlCp4EhN6Mtmm");
+	       	handler.endorseRequest(conn);
+	       	conn.connect();
+	       	
+	    	// callback
+	    	
+	    	logger.info("************** opening url:" + eventUrl);
+	       	//OAuthConsumer consumer = new DefaultOAuthConsumer("bijoux-8197", "RHDwlCp4EhN6Mtmm");        
+	    	//URL url = new URL(eventUrl);
+	    	//HttpURLConnection request = (HttpURLConnection) url.openConnection();
+	    	//consumer.sign(request);
+	    	//request.connect();    	
+	
+	    	logger.info("About to save!");
+	
+	    	
+	    	// TODO: save order
+	    	
+	    	// read xml
+	    	String type = conn.getContentType();
+	    	String message = conn.getResponseMessage();
+	    	
+	    	
+	    	logger.info("Content type is " + type);
+	    	logger.info("Content message is " + message);
+	    	
+	    	// send answer:
+	    	/*
+	    	<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+	    	<result>
+	    	    <success>true</success>
+	    	    <message>Account creation successful</message>
+	    	    <accountIdentifier>new-account-identifier</accountIdentifier>
+	    	</result>
+	*/
+	    	//or for errors:http://info.appdirect.com/developers/docs/event_references/api_error_codes/ 
+	    	
+	    	result = new Result(true, "Account creation successful", "fake124");
+	    	
+       	}
+       	else {
+       		result = new Result(false, EventErrorCode.UNAUTHORIZED, "");
+       	}
     	/*
     	 * <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <result>
@@ -81,7 +116,7 @@ For more information about the supported error codes, see the error code documen
     }
     
     /**
-     * <p>Buy event.</p>
+     * <p>Cancel event.</p>
      * 
      * <p>Expected HTTP GET and request '/event/buy'.</p>
      */
@@ -94,13 +129,19 @@ For more information about the supported error codes, see the error code documen
     	// TODO: verify encryption
     	
     	// callback
-    	OAuthConsumer consumer = new DefaultOAuthConsumer("bijoux-8197", "RHDwlCp4EhN6Mtmm");
+    	//OAuthConsumer consumer = new DefaultOAuthConsumer("bijoux-8197", "RHDwlCp4EhN6Mtmm");
     	
     	logger.info("************** opening url:" + eventUrl);
-    	URL url = new URL(eventUrl);
-    	HttpURLConnection request = (HttpURLConnection) url.openConnection();
-    	consumer.sign(request);
-    	request.connect();    	
+    	//URL url = new URL(eventUrl);
+    	//HttpURLConnection request = (HttpURLConnection) url.openConnection();
+    	//consumer.sign(request);
+    	//request.connect();    	
+    	
+       	URL url = new URL(eventUrl);
+       	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+       	OAuthSingleConsumerURLConnectionAuthHandler handler = new OAuthSingleConsumerURLConnectionAuthHandler("bijoux-8197", "RHDwlCp4EhN6Mtmm");
+       	handler.endorseRequest(conn);
+       	conn.connect();    	
 
     	logger.info("About to save!");
 
@@ -108,7 +149,7 @@ For more information about the supported error codes, see the error code documen
     	// TODO: cancel order
     	
     	// read xml
-    	String type = request.getContentType();
+    	String type = conn.getContentType();
     	
     	logger.info("Content type is " + type);
     	
--- src/main/webapp/WEB-INF/views/subscriptionOrder.jsp
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<result>
-    <success>true</success>
-    <message>Account creation successful for Fake Co. by Alice</message>
-    <accountIdentifier>fakeco123</accountIdentifier>
-</result>
